de9cfb582b480682645cf42a70203f20a436b458
Rob Rosario <robbysrosario@gmail.com>
2016-03-08 12:15:58 -0800

    integrated bootstrap4, tether, and fixed jquery DOM issue.  jquery and electron has a bug where we must append it to the window

--- app/index.js
+++ app/index.js
@@ -3,11 +3,14 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+global.ipcRenderer = require('electron').ipcRenderer;
 
 var mainWindow = null;
+// require('bootstrap');
 
 app.on('ready', function(){
   mainWindow = new BrowserWindow({width:750,height:550});
+  // window.$ = window.jQuery = require('node_modules/jquery/dist/jquery.js');
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   mainWindow.webContents.openDevTools();
