c16fd938be6a8d4bf8239aecd647ee14ef3dbd8a
dvdschwrtz <dbschwartz@protonmail.com>
2016-03-14 16:19:10 -0700

    search file

--- app/js/file-search.js
+++ app/js/file-search.js
@@ -1,43 +1,89 @@
-var fs = require('fs');
-var path = require('path');
-window.$ = window.jQuery = require('./node_modules/jquery/dist/jquery.js');
+'use strict'
+// why?
+// window.$ = window.jQuery = require('./node_modules/jquery/dist/jquery.js');
+const fs = require('fs');
+const path = require('path');
 
+const walk = (baseFolder, search, exclude, done) => {
+  let results = [];
+  fs.readdir(baseFolder, (err, list) => {
+    if (err) return done(err);
 
-var fileTree = [];
-
-function getFilesRecursive (folder, search, exclude) {
-    var fileContents = fs.readdirSync(folder),
-        stats;
-
-    fileContents.forEach(function (fileName) {
-        if (!exclude.hasOwnProperty(fileName)) {
-          stats = fs.lstatSync(folder + '/' + fileName);
-        if (stats.isDirectory()) {
-          if (fileName.indexOf(search.value) && search.folder) {
-            fileTree.push(folder + '/' + fileName);
-          }
-          getFilesRecursive(folder + '/' + fileName, search, exclude);
-        } else {
-          if (fileName.indexOf(search.value) > -1 && !(search.folder)) {
-            fileTree.push(folder + '/' + fileName);
-          }
-        }
-      }
+    list = list.filter(listItem => {
+      return !exclude[listItem];
     });
 
-    return fileTree;
+    if (list.indexOf(search.value) !== -1) {
+      results.push(path.resolve(baseFolder, search.value));
+      return results;
+    }
+
+    let pending = list.length;
+    if (!pending) return done(null, results);
+
+    list.forEach(file => {
+      let filePath = path.resolve(baseFolder, file);
+
+      fs.stat(filePath, (err, stat) => {
+        if (stat && stat.isDirectory()) {
+
+          walk(filePath, search, exclude, (err, res) => {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        }
+          if (!--pending) done(null, results);
+      });
+    });
+  });
 };
 
-var searchParam = {
-  value: "index",
+
+let searchParam = {
+  value: "index.html",
   folder: false
 };
 
-var excludeParam = {
+let excludeParam = {
   ".git": true,
   ".gitignore": true
-}
+};
 
-console.log(getFilesRecursive(path.join(__dirname, "./../unit-project-api"), searchParam, excludeParam));
+walk(process.env.HOME, searchParam, excludeParam, function(err, results) {
+  if (err) throw err;
+  console.log(results);
+});
 
-console.log(__dirname);
+
+//
+// var fileTree = [];
+//
+// function getFilesRecursive (folder, search, exclude, done) {
+//     var stats;
+//     var fileContents = fs.readdir(folder, done),
+//
+//     fileContents.forEach(function (fileName) {
+//         if (!exclude.hasOwnProperty(fileName)) {
+//           stats = fs.lstat(folder + '/' + fileName);
+//         if (stats.isDirectory()) {
+//           if (fileName.indexOf(search.value) && search.folder) {
+//             fileTree.push(folder + '/' + fileName);
+//           }
+//           getFilesRecursive(folder + '/' + fileName, search, exclude);
+//         } else {
+//           if (fileName.indexOf(search.value) > -1 && !(search.folder)) {
+//             fileTree.push(folder + '/' + fileName);
+//           }
+//         }
+//       }
+//     });
+//
+//     return fileTree;
+// };
+//
+// console.log(getFilesRecursive(path.join(__dirname, "./../../../unit-project-api"), searchParam, excludeParam, function(err, results) {
+//   if (err) throw err;
+//   console.log(results);
+// }));
+//
+// console.log(__dirname);
