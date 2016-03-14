0c4b342059c3e866759a8200dc426ae54b0ef752
Rob Rosario <robbysrosario@gmail.com>
2016-03-14 11:38:50 -0700

    electron app restarted, and basic UI added

--- app/index.js
+++ app/index.js
@@ -6,11 +6,9 @@ const BrowserWindow = electron.BrowserWindow;
 global.ipcRenderer = require('electron').ipcRenderer;
 
 var mainWindow = null;
-// require('bootstrap');
 
 app.on('ready', function(){
-  mainWindow = new BrowserWindow({width:750,height:550});
-  // window.$ = window.jQuery = require('node_modules/jquery/dist/jquery.js');
+  mainWindow = new BrowserWindow({width:750,height:550, titleBarStyle: "hidden-inset"});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   mainWindow.webContents.openDevTools();
