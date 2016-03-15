05d45968e5f0cf6089c6e679d3cdc8f45a8ca2d1
Rob Rosario <robbysrosario@gmail.com>
2016-03-15 13:48:27 -0700

    refactored react components.

--- app/src/components/app.js
+++ app/src/components/app.js
@@ -2,11 +2,12 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-
+// PARENT////////////////////////////////////
+// NOTE : The parent will be the electron Window
+// /////////////////////////////////////////////
 let App = React.createClass({
-
   getInitialState: function() {
-    return {windowWidth: window.innerWidth};
+    return {projects: []};
   },
   // addProject: function() {
   //   return (<li className="list-group-item project">
@@ -22,105 +23,146 @@ let App = React.createClass({
   // }
 
   render: function(){
-    return(
-      				<div className="pane-group">
-      					<SideMenu/>
-                <ProjectDetails/>
+    return(<div className="pane-group">
+    					<SideMenu/>
+              <ProjectList/>
               {console.log('react yo')}
-      				</div>
-    )
+    			 </div>)
   }
 });
 
+
+// SIDEMENU ////////////////////////////////////
+// NOTE : Ul element where the side navigation and project list will populate
+// Relationship: App > Sidemenu
+// Children: TopNavList, ProjectList, BottomNavList
+// /////////////////////////////////////////////
 let SideMenu = React.createClass({
-  handleAddProject: function(){
+  handleAddProject: function(e){
     console.log('created project');
   },
 
   render: function () {
     return (
        <div className="pane-sm sidebar">
-         <ul className="list-group container-list container-links">
-         <TopNav/>
-         <ProjectItem/>
-         <BottomNav/>
-         </ul>
+         <TopNavList/>
+         <ProjectList/>
+         <BottomNavList/>
        </div>
     )
   }
 });
 
-let TopNav = React.createClass({
-  render: function () {
-    return (<li className="list-group-item add-container">
-      <button type="button" name="button" onClick="">
-        <span className=" icon ion-ios-plus-outline"></span>
-      </button>
-    </li>)
+
+let TopNavList = React.createClass({
+  render : function(){
+
+    return ( <ul className="list-group container-list container-links topNav">
+               <li className="list-group-item add-container">
+                  <button type="button" name="button" onClick="">
+                    <span className=" icon ion-ios-plus-outline"></span>
+                  </button>
+               </li>
+             </ul> )
   }
 });
 
-let ProjectItem = React.createClass({
+// let TopNavItem = React.createClass({
+//   render: function () {
+//     return (<li className="list-group-item add-container">
+//       <button type="button" name="button" onClick="">
+//         <span className=" icon ion-ios-plus-outline"></span>
+//       </button>
+//     </li>)
+//   }
+// });
+
+let ProjectList = React.createClass({
   render: function () {
-    return (<li className="list-group-item active-projects">
-      <div className="media-body project">
+
+    if (this.props.projects) {
+      var children = this.props.projects.map((x)=> {
+        return <li className="list-group-item active-projects">
+        <div className="media-body project">
         <div className="col-xs-2">
         </div>
         <div className="col-xs-10">
-          <strong>List item title</strong>
-        </div>
+        <strong>{this.props.projects.name}</strong>
       </div>
-    </li>)
-  }
-});
-
-let ProjectDetails = React.createClass({
-  render: function(){
-    return (<div className="pane" id="main">
-      <div className="container main-content-wrapper">
-        <div className="card-group main-content-card-group ">
-          <ProjectDetailItem/>
-        </div>
       </div>
-    </div>)
+      </li>
+    });
+
+    }
+
+    return ( <ul className=" list-group container-list container-links projects"> {children} </ul> )
   }
 });
 
-let ProjectDetailItem = React.createClass({
+let BottomNavList = React.createClass({
   render: function(){
-    return (<div className="card dependancy col-xs-12">
-              <div className="card-block">
-                <h4 className="card-title">Title</h4>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">...</small></p>
-              </div>
-            </div>)
-  }
-});
 
-let BottomNav = React.createClass({
-  render: function(){
-    return(  <ul className="list-group container-list bottom-nav">
-              <li className="list-group-item options">
-                <div className="col-xs-4">
-                </div>
-                <div className="col-xs-4">
-                </div>
-                <div className="col-xs-4">
-                  <SettingsBtn/>
-                </div>
+  return(  <ul className="list-group container-list bottom-nav">
+              <li className="list-group-item add-container">
+                <button type="button" name="button" onClick="">
+                  <span className=" icon ion-gear-a"></span>
+                </button>
               </li>
             </ul>)
+}
+});
+
+// PROJECTLIST ///////////////////////////////////
+// NOTE : All active projects will be populated here
+// /////////////////////////////////////////////
+let ProjectDetailList = React.createClass({
+  render: function(){
+    if (this.props.projects) {
+      var children = this.props.projects.map((x)=> {
+        return (<div className="card dependancy col-xs-12">
+                <div className="card-block">
+                <h4 className="card-title">Title</h4>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text"><small className="text-muted">...</small></p>
+          </div>
+          </div>)
+        });
+
+        return (<div className="pane" id="main">
+                  <div className="container main-content-wrapper">
+                    <div className="card-group main-content-card-group ">
+                    { children }
+                    </div>
+                  </div>
+                </div>)
+    }
+    else {
+      return
+    }
+
   }
 });
 
-let SettingsBtn = React.createClass({
-  render: function(){
-    return (<button type="button" className="settings-btn">
-              <span className="ion-ios-gear-outline"></span>
-            </button>)
-  }
-});
+// let ProjectDetailItem = React.createClass({
+//   render: function(){
+//     return (<div className="card dependancy col-xs-12">
+//               <div className="card-block">
+//                 <h4 className="card-title">Title</h4>
+//                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//                 <p className="card-text"><small className="text-muted">...</small></p>
+//               </div>
+//             </div>)
+//   }
+// });
+
+
+// let SettingsBtn = React.createClass({
+//   render: function(){
+//     return (<button type="button" className="settings-btn">
+//               <span className="ion-ios-gear-outline"></span>
+//             </button>)
+//   }
+// });
 
 
 
