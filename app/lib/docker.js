672f6551125737ab0a04182a6201c519a690afe9
Sam Hagan <samuel.hagan@gmail.com>
2016-03-15 06:32:44 -0700

    working on file watcher

--- app/lib/docker.js
+++ app/lib/docker.js
@@ -1,47 +1,7 @@
 'use strict';
 
-var _utils = require('./utils.js');
+var _requestPromise = require('request-promise');
 
-const docker = (0, _utils.cmdLine)('docker');
+var _requestPromise2 = _interopRequireDefault(_requestPromise);
 
-const dockerStart = container_name => {
-  return docker(`start ${ container_name }`);
-};
-
-dockerStart('angry_heisenberg').then(console.log);
-
-const dockerStop = container_name => {
-  docker(['stop', container_name]);
-};
-
-// alternatively we can use docker create and then docker start
-const dockerRun = (network, image_name) => {
-  docker(['run', '-itd', `'--net=${ network }'`, image_name]);
-};
-
-const dockerSearch = imageSearch => {
-  docker(['search', imageSearch]);
-};
-
-// will compose with dockerSearch
-const dockerPull = imageToPull => {
-  docker(['pull', imageToPull]);
-};
-
-// what other options to add for this?
-const dockerCommit = (container_id, image_name, tag) => {
-  docker(['commit', container_id, `'${ image_name }:${ tag }'`]);
-};
-
-// can't get rsync to copy images - this is slow but it works
-// how to manage password input for automated process??
-const copyImages = (image_name, user, ip_address) => {
-  docker([`save ${ image_name } | bzip2 |  ssh ${ user }@${ ip_address } 'bunzip2 | docker load'`]);
-};
-
-// things to work on:
-
-// 1: creating docker networks
-const dockerNetworkCreate = newNetworkName => {
-  docker(['network', 'create', '-d', 'bridge', newNetworkName]);
-};
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- app/lib/fileWatch.js
+++ app/lib/fileWatch.js
@@ -1,9 +1,31 @@
 'use strict';
 
-const chokidar = require('chokidar');
+var _path = require('path');
 
-const watcher = chokidar.watch(__dirname + '/dir1');
+var _chokidar = require('chokidar');
 
-watcher.on('all', (event, path) => {
+var _chokidar2 = _interopRequireDefault(_chokidar);
+
+var _ramda = require('ramda');
+
+var _ramda2 = _interopRequireDefault(_ramda);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const initObj = { name: 'sam' };
+
+const test = { first: initObj };
+
+test.first.watcher = _chokidar2.default.watch((0, _path.join)(__dirname, '..', '..', 'test', 'userFolder', 'syncTest'));
+
+test.first.watcher.on('all', (event, path) => {
   console.log(event, path);
 });
+
+test.first = _ramda2.default.merge(test.first, { name: 'hello' });
+
+console.log(test.first);
+
+console.log(test.first === initObj);
+
+console.log(initObj.watcher === test.first.watcher);
