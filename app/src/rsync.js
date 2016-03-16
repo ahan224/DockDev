3edb1d2ec5f2ce3257a32b9ec7c0a1343a579d7c
Sam Hagan <samuel.hagan@gmail.com>
2016-03-16 14:47:52 -0700

    completed file watch and syncing

--- app/src/rsync.js
+++ app/src/rsync.js
@@ -24,15 +24,22 @@ const selectWithin = R.curry((array, key, obj) => {
 // returns an array of arguments for rsync
 const createRsyncArgs = (source, dest, machineInfo) => {
 
-  return `-a -e
-"ssh -i ${ machine.SSHKeyPath }
--o IdentitiesOnly=yes
--o UserKnownHostsFile=/dev/null
--o StrictHostKeyChecking=no"
---delete ${ source }
-docker@${ machine.IPAddress }:${ dest }`
+  return `-a -e "ssh -i ${ machineInfo.SSHKeyPath } -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" --delete ${ source } docker@${ machineInfo.IPAddress }:${ dest }`
 
 };
+
+
+const source = '/Users/samhagan/dev/codesmith/DockDev/test/userFolder/project5';
+const dest = '/opt/test';
+const sandbox = {
+  SSHKeyPath: '/Users/samhagan/.docker/machine/machines/sandbox/id_rsa',
+  IPAddress: '192.168.99.102'
+}
+
+rsync(createRsyncArgs(source, dest, sandbox))
+  .then(console.log)
+  .catch(console.log);
+
 
 // selectSSHandIP :: object -> object
 // selects ssh and ip address from docker-machine inspect object
@@ -43,7 +50,7 @@ const selectSSHandIP = R.compose(
 
 function getSyncContainer(projObj) {
   for (var container in projObj.containers) {
-    if (container.sync) return container.containerId;
+    if (projObj.containers[container].sync) return projObj.containers[container].containerId;
   }
 }
 
@@ -51,17 +58,29 @@ function getSyncContainer(projObj) {
 // generateRsync :: object -> function
 // accepts a config object and returns a function that is
 // called when files change in the base directory of project
-export const generateRsync = co(function*(projObj) {
-  const machineInfo = selectSSHandIP(yield machine.inspect(projObj.machine));
-  const targetContainerId = getSyncContainer(projObj);
-  const dest = projObj.containers[targetContainerId].dest;
-  const args = createRsyncArgs(projObj.basePath, dest, machineInfo);
+export function generateRsync(projObj) {
+
+  const getArgs = co(function *() {
+    const machineInfo = selectSSHandIP(yield machine.inspect(projObj.machine));
+    const targetContainerId = getSyncContainer(projObj);
+    const dest = projObj.containers[targetContainerId].dest;
+
+    try {
+      yield machine.ssh(projObj.machine, `mkdir ${ dest }`)
+    } catch (e) {
+      // console.log(e);
+    }
+
+    return createRsyncArgs(`${ projObj.basePath }/`, dest, machineInfo);
+  })
+
+  const args = getArgs();
 
   return co(function *() {
-    return yield rsync(args)
+    return yield rsync(yield args);
   })
-  
-})
+
+}
 
 
 // - File watch
