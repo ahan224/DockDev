d9023b567848d28b48aa805a9981523cbe554148
Rob Rosario <robbysrosario@gmail.com>
2016-03-16 15:01:20 -0700

    merged master, rsync, docker, dockermachine

--- app/lib/docker.js
+++ app/lib/docker.js
@@ -2,46 +2,46 @@
 
 var _utils = require('./utils.js');
 
-var docker = (0, _utils.cmdLine)('docker');
+const docker = (0, _utils.cmdLine)('docker');
 
-var dockerStart = function dockerStart(container_name) {
-  return docker('start ' + container_name);
+const dockerStart = container_name => {
+  return docker(`start ${ container_name }`);
 };
 
 dockerStart('angry_heisenberg').then(console.log);
 
-var dockerStop = function dockerStop(container_name) {
+const dockerStop = container_name => {
   docker(['stop', container_name]);
 };
 
 // alternatively we can use docker create and then docker start
-var dockerRun = function dockerRun(network, image_name) {
-  docker(['run', '-itd', '\'--net=' + network + '\'', image_name]);
+const dockerRun = (network, image_name) => {
+  docker(['run', '-itd', `'--net=${ network }'`, image_name]);
 };
 
-var dockerSearch = function dockerSearch(imageSearch) {
+const dockerSearch = imageSearch => {
   docker(['search', imageSearch]);
 };
 
 // will compose with dockerSearch
-var dockerPull = function dockerPull(imageToPull) {
+const dockerPull = imageToPull => {
   docker(['pull', imageToPull]);
 };
 
 // what other options to add for this?
-var dockerCommit = function dockerCommit(container_id, image_name, tag) {
-  docker(['commit', container_id, '\'' + image_name + ':' + tag + '\'']);
+const dockerCommit = (container_id, image_name, tag) => {
+  docker(['commit', container_id, `'${ image_name }:${ tag }'`]);
 };
 
 // can't get rsync to copy images - this is slow but it works
 // how to manage password input for automated process??
-var copyImages = function copyImages(image_name, user, ip_address) {
-  docker(['save ' + image_name + ' | bzip2 |  ssh ' + user + '@' + ip_address + ' \'bunzip2 | docker load\'']);
+const copyImages = (image_name, user, ip_address) => {
+  docker([`save ${ image_name } | bzip2 |  ssh ${ user }@${ ip_address } 'bunzip2 | docker load'`]);
 };
 
 // things to work on:
 
 // 1: creating docker networks
-var dockerNetworkCreate = function dockerNetworkCreate(newNetworkName) {
+const dockerNetworkCreate = newNetworkName => {
   docker(['network', 'create', '-d', 'bridge', newNetworkName]);
 };
--- app/lib/dockerMachine.js
+++ app/lib/dockerMachine.js
@@ -12,22 +12,18 @@ var _utils = require('./utils.js');
 // dockerMachine :: string -> promise(string)
 // accepts an array of cmd line args for docker-machine
 // returns a promise that resolves to the stdout
-var dockerMachine = exports.dockerMachine = (0, _utils.cmdLine)('docker-machine');
+const dockerMachine = exports.dockerMachine = (0, _utils.cmdLine)('docker-machine');
 
 // creates a Droplet on DigitalOcean
 //** promisify this function
-var createDroplet = function createDroplet(accessToken, dropletName) {
+const createDroplet = (accessToken, dropletName) => {
   dropletName = "test9";
   accessToken = 'eedf80c21a790ed8328a1f64447a2b239ba98c8137051a362b8bee89530968a7';
   return (0, _fs.spawn)('docker-machine', ['create', '--driver', 'digitalocean', '--digitalocean-access-token', accessToken, dropletName]);
 };
 
 // also stops a Droplet on digitalocean
-var stopMachine = function stopMachine(machineName) {
-  return dockerMachine('stop ' + machineName);
-};
+const stopMachine = machineName => dockerMachine(`stop ${ machineName }`);
 
 // also removes a Droplet on DigitalOcean
-var removeMachine = function removeMachine(machineName) {
-  return dockerMachine('rm -y ' + machineName);
-};
+const removeMachine = machineName => dockerMachine(`rm -y ${ machineName }`);
