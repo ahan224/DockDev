2b6e1d49b7c1a0972dcc225bb4ec65d3ba76e6b4
Rob Rosario <robbysrosario@gmail.com>
2016-03-18 14:01:45 -0700

    reset hard done.  the last merge was not reflecting the updates

--- app/main.js
+++ app/main.js
@@ -36,7 +36,6 @@ app.on('ready', function () {
 
 ipcMain.on('asynchronous-message', function (event, arg) {
   console.log(arg); // prints "ping"
-
   event.sender.send('asynchronous-reply', 'pong');
 });
 
