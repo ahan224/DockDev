a1de7745ddb7434b0e469e628943cf28ecaf10bc
Rob Rosario <robbysrosario@gmail.com>
2016-03-21 16:29:12 -0700

    components setup but routes are breaking somehow

---
+++ app/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import * as utils from './lib/utils.js';
+// import About from './components/AddProject';
+import { Router, Route, hashHistory } from 'react-router'
+
+import Home from './src/components/Home.js';
+import AddProject from './src/components/AddProject.js';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.addProject = this.addProject.bind(this);
+    this.projectSelect = this.projectSelect.bind(this);
+    this.state = {
+      projects: {
+        first: {
+          projectName: 'test',
+          uuid: 1234556667
+        }
+      }
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('state updated', this.state);
+  }
+
+  addProject(e) {
+    remote.dialog
+      .showOpenDialog(
+        { properties: ['openDirectory', 'createDirectory'] },
+        selectedDir => {
+          const name = selectedDir[0].split('/').pop();
+          return utils.initProject(selectedDir[0], name)
+            .then(() => this.updateProject(utils.memory));
+        }
+      );
+  }
+
+  updateProject(memory) {
+    this.setState({ projects: memory });
+  }
+
+  settingsClick(e) {
+  }
+
+  projectSelect(uuid) {
+    console.log(uuid);
+  }
+
+}
+
+ReactDom.render(
+    <Router history={hashHistory}>
+      <Route path="/" component={Home}>
+        <Route path="/addproject" component={AddProject} />
+      </Route>
+    </Router>
+  , document.getElementById('app'));
