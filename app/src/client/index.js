133d7c4a1ea9dc608eb82f96f705dd0471460ca8
Sam Hagan <samuel.hagan@gmail.com>
2016-03-22 08:37:47 -0700

    refactored front-end code

--- app/src/client/index.js
+++ app/src/client/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
-import App from './components/App.js';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from './components/App';
+import Repos from './components/Repos';
+import Repo from './components/Repo';
+import About from './components/About';
+import Home from './components/Home';
+import AddProject from './components/AddProject';
 
 render((
   <Router history={hashHistory}>
-    <Route path="/" component={App} />
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="/repos" component={Repos} >
+        <Route path="/repos/:userName/:repoName" component={Repo} />
+      </Route>
+      <Route path="/about" component={About} />
+      <Route path="/addProject" component={AddProject} />
+    </Route>
   </Router>
 ), document.getElementById('app'));
