98b2060039c61aa55eb26fafddfd8126bbd56cd3
Sam Hagan <samuel.hagan@gmail.com>
2016-03-22 18:14:52 -0700

    working on add project

--- app/src/client/index.js
+++ app/src/client/index.js
@@ -4,15 +4,21 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import AddProject from './components/AddProject';
-import Project from './components/Project';
-// import Projects from './components/Projects';
+import ProjectDetails from './components/ProjectDetails';
+import ProjectSettings from './components/ProjectSettings';
+import ProjectDeploy from './components/ProjectDeploy';
+import ProjectNav from './components/ProjectNav';
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/addProject" component={AddProject} />
-      <Route path="/projects/:id" component={Project} />
+      <Route path="/projects/:uuid" component={ProjectNav} >
+        <IndexRoute component={ProjectDetails} />
+        <Route path="/projects/:uuid/settings" component={ProjectSettings} />
+        <Route path="/projects/:uuid/deploy" component={ProjectDeploy} />
+      </Route>
     </Route>
   </Router>
 ), document.getElementById('app'));
