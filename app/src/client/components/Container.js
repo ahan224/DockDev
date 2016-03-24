8b77be50ee9aa4ccee8e220935b4cdc232a5d8d2
Sam Hagan <samuel.hagan@gmail.com>
2016-03-24 07:57:50 -0700

    working on add container logic

---
+++ app/src/client/components/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Container = (props) => {
+  return (
+    <div>
+      Container
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Restart</button>
+      <button>Delete</button>
+    </div>
+  );
+};
+
+export default Container;
