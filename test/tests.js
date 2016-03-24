7f99a08d28f4f278740c6cbc673c6f47fe0842bb
Alan Han <alanhan224@gmail.com>
2016-03-24 09:45:32 -0700

    stored changes to test

--- test/tests.js
+++ test/tests.js
@@ -1,23 +1,25 @@
 import { expect } from 'chai';
-import * as utils from '../app/lib/utils.js';
+// import * as utils from '../app/build/server/utils.js';
 import { join } from 'path';
 import rimraf from 'rimraf';
 import { readFileSync, mkdirSync, readdirSync, writeFileSync } from 'fs';
 import R from 'ramda';
-import * as rsync from '../app/lib/rsync.js';
-import { addFileWatcher } from '../app/lib/fileWatch.js';
-import { removeContainer, addContainer } from '../app/lib/container.js';
+import * as rsync from '../app/build/server/rsync.js';
+import { addFileWatcher } from '../app/build/server/fileWatch.js';
+import { removeContainer, addContainer } from '../app/build/server/container.js';
+import * as projConfig from '../app/build/server/projConfig.js';
+import * as defaultConfig from '../app/build/server/defaultConfig.js';
 
 describe('initiate new DockDev project via individual functions', () => {
-  const projectName = 'project1'
+  const projectName = 'project1';
   const basePath = join(__dirname, 'userFolder', projectName);
-  const dockDevPath = join(basePath, '.dockdev');
+  // const dockDevPath = join(basePath, '.dockdev');
   let result;
   let projObj;
 
   before(() => {
     // make sure there is a userFolder
-    try { mkdirSync(join(__dirname, 'userFolder')) }
+    try { mkdirSync(join(__dirname, 'userFolder')); }
     catch (e) {}
 
     // remove project projFolder if it exists
@@ -28,52 +30,47 @@ describe('initiate new DockDev project via individual functions', () => {
   });
 
   it('createProj should create a config object with a unique id and project name', () => {
-    projObj = utils.createProj(basePath, projectName);
+    projObj = projConfig.createProj(basePath, projectName);
     expect(projObj.projectName).to.equal(projectName);
     expect(projObj.uuid).to.be.a('string');
     expect(projObj.basePath).to.be.a('string');
     expect(projObj.basePath).to.equal(basePath);
-  })
+  });
 
   it('createDockDev should create .dockdev projFolder when none exists', () => {
-    result = utils.createDockDev(projObj);
+    result = projConfig.createDockDev(projObj);
     return result.then(() => {
-      expect(readdirSync(join(projObj.basePath, utils.config.projFolder))).to.be.empty;
+      expect(readdirSync(join(projObj.basePath, defaultConfig.projFolder))).to.be.empty;
     });
   });
 
   it('writeProj should write a specified object to the configFile', () => {
-    return utils.writeProj(projObj)
+    return projConfig.writeProj(projObj)
       .then(() => readFileSync(join(projObj.basePath, '.dockdev', 'dockdev.json')))
       .then(R.toString)
       .then(JSON.parse)
-      .then(data => expect(data).to.deep.equal(R.pick(utils.config.projWriteParams, projObj)));
-  })
-
-  it('addProjToMemory should add the config object to the apps memory object', () => {
-    utils.addProjToMemory(utils.memory, projObj);
-    expect(utils.memory[projObj.uuid]).to.equal(projObj);
-  })
+      .then(data => expect(data).to.deep.equal(R.pick(defaultConfig.projWriteParams, projObj)));
+  });
 
   it('createDockDev should fail when the projFolder already exists', () => {
-    const tryAgain = utils.createDockDev(projObj);
+    const tryAgain = projConfig.createDockDev(projObj);
     return tryAgain
     .then(
       data => expect(data).to.equal(undefined),
       err => expect(err.code).to.equal('EEXIST')
-    )
-  })
+    );
+  });
 });
 
 describe('initiate new DockDev project via initiateProject', () => {
-  const projectName = 'project2'
+  const projectName = 'project2';
   const basePath = join(__dirname, 'userFolder', projectName);
-  const dockDevPath = join(basePath, '.dockdev');
+  // const dockDevPath = join(basePath, '.dockdev');
   let result;
 
   before(() => {
     // make sure there is a userFolder
-    try { mkdirSync(join(__dirname, 'userFolder')) }
+    try { mkdirSync(join(__dirname, 'userFolder')); }
     catch (e) {}
 
     // remove project projFolder if it exists
@@ -84,7 +81,7 @@ describe('initiate new DockDev project via initiateProject', () => {
   });
 
   it('should create a projObj', () => {
-    result = utils.initProject(basePath, projectName);
+    result = projConfig.initProject(basePath, projectName);
     return result
       .then(data => {
         expect(data).to.be.an('object');
@@ -92,38 +89,33 @@ describe('initiate new DockDev project via initiateProject', () => {
         expect(data.projectName).to.equal(projectName);
         expect(data.basePath).to.equal(basePath);
       });
-  })
+  });
 
   it('should write the config file to dockdev.json', () => {
     return result
       .then(() => readFileSync(join(basePath, '.dockdev', 'dockdev.json')))
       .then(R.toString)
       .then(JSON.parse)
-      .then(data => result.then(orig => expect(R.pick(utils.config.projWriteParams, orig)).to.deep.equal(data)))
-  })
-
-  it('should add the config to app memory', () => {
-    return result
-      .then(data => expect(data).to.equal(utils.memory[data.uuid]))
-  })
+      .then(data => result.then(orig =>
+        expect(R.pick(defaultConfig.projWriteParams, orig)).to.deep.equal(data)));
+  });
 
   it('should fail if a project already exists', () => {
-    return utils.initProject(basePath, projectName)
+    return projConfig.initProject(basePath, projectName)
       .then(data => expect(data).to.be(undefined))
-      .catch(err => expect(err.code).to.equal('EEXIST'))
-  })
-
-})
+      .catch(err => expect(err.code).to.equal('EEXIST'));
+  });
+});
 
 describe('read and modify an existing project', () => {
-  const projectName = 'project3'
+  const projectName = 'project3';
   const basePath = join(__dirname, 'userFolder', projectName);
-  const dockDevPath = join(basePath, '.dockdev');
+  // const dockDevPath = join(basePath, '.dockdev');
   let result;
 
   before(() => {
     // make sure there is a userFolder
-    try { mkdirSync(join(__dirname, 'userFolder')) }
+    try { mkdirSync(join(__dirname, 'userFolder')); }
     catch (e) {}
 
     // remove project projFolder if it exists
@@ -132,17 +124,16 @@ describe('read and modify an existing project', () => {
     // add back the project projFolder
     mkdirSync(basePath);
 
-    result = utils.initProject(basePath, projectName);
+    result = projConfig.initProject(basePath, projectName);
   });
 
   // this should probably be moved to the existing project tests (project2)
   it('readProj should read an existing config file returning an object', () => {
     return result
-    .then(data => utils.readProj(data.basePath))
-    .then(data => result.then(orig => expect(data).to.deep.equal(orig)))
-  })
-
-})
+    .then(data => projConfig.readProj(data.basePath))
+    .then(data => result.then(orig => expect(data).to.deep.equal(orig)));
+  });
+});
 
 // describe('find our target files in specified directory', () => {
 //
@@ -151,16 +142,16 @@ describe('read and modify an existing project', () => {
 
 
 describe('add and modify containers within a project', () => {
-  const projectName = 'project4'
+  const projectName = 'project4';
   const basePath = join(__dirname, 'userFolder', projectName);
-  const dockDevPath = join(basePath, '.dockdev');
+  // const dockDevPath = join(basePath, '.dockdev');
   const image = 'node';
   let result;
   let containerId;
 
   before(() => {
     // make sure there is a userFolder
-    try { mkdirSync(join(__dirname, 'userFolder')) }
+    try { mkdirSync(join(__dirname, 'userFolder')); }
     catch (e) {}
 
     // remove project projFolder if it exists
@@ -169,7 +160,7 @@ describe('add and modify containers within a project', () => {
     // add back the project projFolder
     mkdirSync(basePath);
 
-    result = utils.initProject(basePath, projectName);
+    result = projConfig.initProject(basePath, projectName);
   });
 
   it('should add a container to the project', () => {
@@ -181,9 +172,9 @@ describe('add and modify containers within a project', () => {
         result.then(data => {
           expect(data.containers[containerId].image).to.equal(image);
           expect(data.containers[containerId].containerId).to.equal(containerId);
-        })
+        });
       });
-  })
+  });
 
   it('should delete a container from a project', () => {
     return result
@@ -191,22 +182,22 @@ describe('add and modify containers within a project', () => {
       .then(data => {
         expect(data).to.equal(true);
         expect(data.containers).to.be.empty;
-      })
-  })
-})
+      });
+  });
+});
 
 
 describe('should sync files to docker machine', () => {
-  const projectName = 'project5'
+  const projectName = 'project5';
   const basePath = join(__dirname, 'userFolder', projectName);
-  const dockDevPath = join(basePath, '.dockdev');
+  // const dockDevPath = join(basePath, '.dockdev');
   const image = 'node';
   let result;
   let containerId;
 
   before(() => {
     // make sure there is a userFolder
-    try { mkdirSync(join(__dirname, 'userFolder')) }
+    try { mkdirSync(join(__dirname, 'userFolder')); }
     catch (e) {}
 
     // remove project projFolder if it exists
@@ -215,11 +206,11 @@ describe('should sync files to docker machine', () => {
     // add back the project projFolder
     mkdirSync(basePath);
 
-    containerId = utils.initProject(basePath, projectName)
+    containerId = projConfig.initProject(basePath, projectName)
       .then(data => {
         result = data;
-        return addContainer(data, image)
-      })
+        return addContainer(data, image);
+      });
   });
 
   it('should sync folder to docker-machine', () => {
@@ -228,8 +219,8 @@ describe('should sync files to docker machine', () => {
         const syncFunc = rsync.generateRsync(result);
         writeFileSync(join(result.basePath, 'test.txt'));
         return syncFunc();
-      })
-  })
+      });
+  });
 
   it('should watch and sync files to the docker-machine', (done) => {
     return containerId
@@ -243,26 +234,26 @@ describe('should sync files to docker machine', () => {
 
         setTimeout(done, 1900);
         expect(true).to.be.true;
-        return ;
-      })
-  })
-})
+        return;
+      });
+  });
+});
 
 xdescribe('should write a config file', () => {
   let result;
-  const basePath = join(__dirname, 'configFolder')
+  const basePath = join(__dirname, 'configFolder');
 
 // need to delete the configFolder and File before, then run the
   before(() => {
-    try { mkdirSync(basePath) }
+    try { mkdirSync(basePath); }
     catch (e) {}
 
     // remove configFolder if it exists
     rimraf.sync(basePath);
 
     // will create in basePath and also searches for projectFolders in the test directory
-    utils.writeConfig(__dirname, basePath);
-    result = readFileSync(utils.config.configPath(basePath));
+    projConfig.writeConfig(__dirname, basePath);
+    result = readFileSync(defaultConfig.configPath(basePath));
   });
 
   // this should probably be moved to the existing project tests (project2)
@@ -271,7 +262,6 @@ xdescribe('should write a config file', () => {
       .then(data => {
         expect(data.userSelectedDirectory).to.equal(process.env.HOME);
         expect(data.projects).to.be.empty;
-      })
-  })
-
+      });
+  });
 });
