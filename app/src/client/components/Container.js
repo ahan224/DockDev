0d964e88a36d77354582d85dc92f60d53aaf4ae8
Alan Han <alanhan224@gmail.com>
2016-03-25 13:47:04 -0700

    Reformatted add containers to preset buttons

--- app/src/client/components/Container.js
+++ app/src/client/components/Container.js
@@ -2,19 +2,15 @@ import React from 'react';
 
 const Container = (props) => {
   return (
-    <li>
-      <ul>
-        <li>
-          <h3>{props.details[1].image}</h3>
-        </li>
-        <li>
-          <p>Some description here.</p>
-        </li>
-      </ul>
-      <button>Start</button>
-      <button>Stop</button>
-      <button>Restart</button>
-      <button>Delete</button>
+    <li className="container-list">
+      <div className="container-box">
+        <h4 className="text-container">{ props.details[1].image }</h4>
+        <p className="text-version">v1.1.1.0</p>
+        <p className="text-description">
+          A Docker project management tool that automatically syncs files,
+          seamlessly integrate Docker with existing workflow, and
+          manage Docker projects and easily deploy them</p>
+      </div>
     </li>
   );
 };
