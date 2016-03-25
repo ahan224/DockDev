efdf0ded7fb45878423dccf69c634592044e2021
Rob Rosario <robbysrosario@gmail.com>
2016-03-25 16:13:43 -0700

    new ui added for projectDetails, addProject, addContainer,

--- app/src/client/components/ProjectDetails.js
+++ app/src/client/components/ProjectDetails.js
@@ -7,21 +7,28 @@ const ProjectDetail = ({ projects, params }) => {
   const containers = R.toPairs(proj.containers)
     .map(container => <Container key={container[0]} details={container} />);
   return (
-    <div className="project-details">
+    <div className="col-xs-12">
       <h1 className="display-4 text-capitalize">
         {proj.projectName} Details
       </h1>
-      <button>Start</button>
+      {/*<button>Start</button>
       <button>Stop</button>
       <button>Restart</button>
-      <button>Delete</button>
+      <button>Delete</button>*/}
       <p className="lead">
         All the awesome information about your project,
         container, are right here.  Take a look around.
       </p>
-      <ul>
+      <div className="row">
+        <div className="col-xs-12" id="servers">
+          <h4>Servers</h4>
+          <div className="divider"></div>
+        </div>
         {containers}
-      </ul>
+      </div>
+      <div className="row" id="databases">
+
+      </div>
     </div>
   );
 };
