d31c66bc5cc8e7438b887894bc504b868924ad6d
David Schwartz <dbs2162@columbia.edu>
2016-04-03 14:21:37 -0700

    reorganized front end code - still need to work on the active project button and also putting in content for the different containers

--- app/src/client/components/appComp/TopNav.js
+++ app/src/client/components/appComp/TopNav.js
@@ -7,23 +7,23 @@ const TopNav = ({ activeProject }) => (
 
      <label className="btn btn-primary active">
        <input type="radio" name="options" id="option1" autoComplete="off" />
-         <Link className="nav-link" to={'/settings'}>
-           <img src="./client/images/png/music@2x.png"></img>
-         </Link>
+       <Link className="nav-link" to={'/settings'}>
+         <img src="./client/images/png/music@2x.png"></img>
+       </Link>
      </label>
 
-     <label className="btn btn-primary">
+     <label className="btn btn-primary activeProj">
        <input type="radio" name="options" id="option2" autoComplete="off" />
-         <Link className="nav-link" to={`/projects/${activeProject}`}>
-           <img src="./client/images/png/power@2x.png"></img>
-         </Link>
+       <Link className="nav-link" to={`/projects/${activeProject}`}>
+         <img src="./client/images/png/power@2x.png"></img>
+       </Link>
      </label>
 
      <label className="btn btn-primary">
        <input type="radio" name="options" id="option3" autoComplete="off" />
-         <Link className="nav-link" to={'/settings'}>
-           <img src="./client/images/png/tool@2x.png"></img>
-         </Link>
+       <Link className="nav-link" to={'/settings'}>
+         <img src="./client/images/png/tool@2x.png"></img>
+       </Link>
      </label>
 
    </div>
