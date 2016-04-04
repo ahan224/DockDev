afd42708154ecb2bdb7128ca2f4acd53b87e5309
Sam Hagan <samuel.hagan@gmail.com>
2016-04-04 10:33:49 -0700

    minor revisions to main.js

--- app/src/main.js
+++ app/src/main.js
@@ -9,11 +9,10 @@ let mainWindow = null;
 
 // electron is listening for when the app is 'ready' to load and will create a new browser window
 app.on('ready', () => {
-
   mainWindow = new BrowserWindow({ width: 950, height: 650 });
   mainWindow.loadURL(join('file://', __dirname, 'index.html'));
 
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
   mainWindow.on('close', () => {
     mainWindow = null;
   });
