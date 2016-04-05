0e54b5f44d843cb38354a230ded6ad9eb4045c20
dvdschwrtz <dbschwartz@protonmail.com>
2016-04-05 11:57:19 -0700

    working on backup MVP features

--- app/src/server/main.js
+++ app/src/server/main.js
@@ -10,8 +10,10 @@ import * as manageProj from './projLevel/manageProj';
 import * as projConfig from './projLevel/projConfig';
 import * as rsync from './projLevel/rsync';
 import * as utils from './utils/utils';
+import * as containerMgmt from './projLevel/containerMgmt';
 
 export {
+  containerMgmt,
   appConfig,
   availableImages,
   defaultConfig,
